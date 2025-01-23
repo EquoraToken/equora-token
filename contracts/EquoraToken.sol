7d86b198df4fd0efbf5fbd236ccac865664724a5
Zack <zackrylangford@gmail.com>
2025-01-23 08:27:12 -0500

    prepped and deployed to mainnet

--- contracts/EquoraToken.sol
+++ contracts/EquoraToken.sol
@@ -4,7 +4,9 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract EquoraToken is ERC20 {
+    uint256 private constant TOTAL_SUPPLY = 8025000000 * 10 ** 18;
+
     constructor() ERC20("Equora", "EQR") {
-        _mint(msg.sender, 1000000 * 10 ** decimals()); // Mint 1,000,000 tokens to the deployer's address
+        _mint(msg.sender, TOTAL_SUPPLY);
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -11,5 +11,9 @@ module.exports = {
             url: `https://sepolia.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
             accounts: [`0x${process.env.PRIVATE_KEY}`],
         },
+        mainnet: {
+            url: `https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+            accounts: [`0x${process.env.PRIVATE_KEY}`],
+        },
     },
 };
